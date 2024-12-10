8c7e977c9050827ffb84dbda58d17cb3221267a5
auberginegine <8280187@philasd.org>
2024-12-09 20:13:52 -0500

    Co-authored-by: AT727 <AT727@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.vision;
 
+import java.io.IOException;
 import java.util.Optional;
+import java.util.function.DoubleSupplier;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
@@ -9,47 +11,80 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class Vision extends SubsystemBase {
+  
+  private static Vision instance = null;
 
-private final PhotonCamera noteCamera;
+  public static Vision getInstance() {
+    if (instance == null)
+      try {
+        instance = new Vision();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
 
-private final PhotonPoseEstimator visionEstimator;
-public Drivetrain swerveDrive;
-public Vision (Drivetrain swerveDrive) {
- this.swerveDrive =  swerveDrive;
-this.noteCamera = new PhotonCamera(DrivetrainConstants.kNoteCameraName);
+    return instance;
+  }
 
-this.visionEstimator =
-    new PhotonPoseEstimator(
-        DrivetrainConstants.kAprilTagFieldLayout,
-        PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-        mainCamera,
-        DrivetrainConstants.kRobotToCameraTransform);
+  private final PhotonCamera noteCamera;
+  
+  private final PhotonPoseEstimator visionEstimator;
+  public Drivetrain swerveDrive;
+  public Vision (Drivetrain swerveDrive) {
+   this.swerveDrive =  swerveDrive;
+   
+  this.noteCamera = new PhotonCamera(DrivetrainConstants.kNoteCameraName);
+  }
+  private Vision() throws IOException {
+    this.swerveDrive = swerveDrive;
+    this.noteCamera = new PhotonCamera(DrivetrainConstants.kNoteCameraName);
+  }
 
- }
-        private void fuseVision() {
-            Optional<EstimatedRobotPose> visionEstimate = visionEstimator.update();
-        
-            if (visionEstimate.isEmpty()) return;
-        
-            this.swerveDrive.addVisionMeasurement(
-                visionEstimate.get().estimatedPose.toPose2d(), visionEstimate.get().timestampSeconds);
-        
-            SmartDashboard.putNumber("vision estimate x", visionEstimate.get().estimatedPose.getX());
-            SmartDashboard.putNumber("vision estimate y", visionEstimate.get().estimatedPose.getY());
-            SmartDashboard.putNumber("vision estimate z", visionEstimate.get().estimatedPose.getZ());
-          }
+  private double getNoteAngle() {
+    PhotonPipelineResult latestResult = this.noteCamera.getLatestResult();
+    if (!latestResult.hasTargets()) {
+      return 0.0;
+    }
 
-          @Override
-          public void periodic() {
-            // this.odometry.update(this.gyro.getRotation2d(), this.getModulePositions());
-            this.fuseVision();
+    PhotonTrackedTarget trackedNote = new PhotonTrackedTarget();
+    return -0.5 * trackedNote.getYaw();
+  }
+  
+  public boolean seesNote() {
+   return this.noteCamera.getLatestResult().hasTargets();
+  }
 
-          }
-          }
+  public Command turnToAngle(double angle) {
+        return turnToAngle(() -> angle);
+  }
+    private Command turnToAngle(DoubleSupplier angleSupplier) {
+
+  return run(() -> {
+        ChassisSpeeds speeds =
+              this.swerveDrive.swerveController.getTargetSpeeds(
+                0,
+                0,
+                Math.toRadians(angleSupplier.getAsDouble()),
+                swerveDrive.getOdometryHeading().getRadians(),
+                swerveDrive.getMaximumVelocity());
+        swerveDrive.driveFieldRelative(speeds);
+      })
+      .until(this.swerveDrive.swerveController.thetaController::atSetpoint);
+  }
+
+  public Command turnToNote() {
+    return this.turnToAngle(this::getNoteAngle);
+  }
+  
+@Override
+  public void periodic() {
+  }
+}       
---
+++ src/main/java/org/robolancers321/BuildConstants.java
@@ -0,0 +1,19 @@
+package org.robolancers321;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "trashbot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "25b60fcdc7b6b24062dbfc9596a2b85462264543";
+  public static final String GIT_DATE = "2024-12-06 17:21:11 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-12-09 19:27:40 EST";
+  public static final long BUILD_UNIX_TIME = 1733790460856L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
