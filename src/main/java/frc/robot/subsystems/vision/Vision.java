65c28fc39859fcd1ad6511e608d63f37bc445e01
auberginegine <8280187@philasd.org>
2024-12-11 17:28:35 -0500

    changed viison thingys - imported pid and a bunch of other things
    - maybe fix yagsl library thing question mark
    Co-authored-by: Raphael Hoang  <Floofyer@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.vision;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.function.DoubleSupplier;
@@ -11,11 +12,15 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
@@ -27,7 +32,8 @@ public class Vision extends SubsystemBase {
     if (instance == null)
       try {
         instance = new Vision();
-      } catch (IOException e) {
+      } 
+        catch (IOException e) {
         e.printStackTrace();
       }
 
@@ -36,16 +42,38 @@ public class Vision extends SubsystemBase {
 
   private final PhotonCamera noteCamera;
   
-  private final PhotonPoseEstimator visionEstimator;
-  public Drivetrain swerveDrive;
+  private final PhotonPoseEstimator vision;
+  private SwerveDrive swerveDrive;
   public Vision (Drivetrain swerveDrive) {
-   this.swerveDrive =  swerveDrive;
-   
+  this.swerveDrive =  swerveDrive; 
   this.noteCamera = new PhotonCamera(DrivetrainConstants.kNoteCameraName);
   }
+  
   private Vision() throws IOException {
     this.swerveDrive = swerveDrive;
     this.noteCamera = new PhotonCamera(DrivetrainConstants.kNoteCameraName);
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+    swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Constants.DrivetrainConstants.kMaxSpeedMetersPerSecond);
+  }
+
+  new HolonomicPathFollowerConfig(
+            new PIDConstants(
+                // 0,0,0
+                DrivetrainConstants.kTranslationP,
+                DrivetrainConstants.kTranslationI,
+                DrivetrainConstants.kTranslationD),
+            new PIDConstants(
+                // 0,0,0
+                DrivetrainConstants.kRotationP,
+                DrivetrainConstants.kRotationI,
+                DrivetrainConstants.kRotationD),
+            DrivetrainConstants.kMaxSpeedMetersPerSecond,
+            0.5
+                * Math.hypot(
+                    DrivetrainConstants.kTrackWidthMeters, DrivetrainConstants.kWheelBaseMeters),
+            new ReplanningConfig()),
+        MyAlliance::isRed,
+        this);
   }
 
   private double getNoteAngle() {
@@ -63,25 +91,33 @@ public class Vision extends SubsystemBase {
   }
 
   public Command turnToAngle(double angle) {
-        return turnToAngle(() -> angle);
-  }
-    private Command turnToAngle(DoubleSupplier angleSupplier) {
-
-  return run(() -> {
-        ChassisSpeeds speeds =
-              this.swerveDrive.swerveController.getTargetSpeeds(
-                0,
-                0,
-                Math.toRadians(angleSupplier.getAsDouble()),
-                swerveDrive.getOdometryHeading().getRadians(),
-                swerveDrive.getMaximumVelocity());
-        swerveDrive.driveFieldRelative(speeds);
-      })
-      .until(this.swerveDrive.swerveController.thetaController::atSetpoint);
+    return turnToAngle(getNoteAngle());
   }
 
   public Command turnToNote() {
-    return this.turnToAngle(this::getNoteAngle);
+    return this.turnToAngle(getNoteAngle());
+  }
+
+  private Translation2d getRelativeNoteLocation() {
+    PhotonPipelineResult latestResult = this.noteCamera.getLatestResult();
+
+    if (!latestResult.hasTargets()) return new Translation2d();
+
+    PhotonTrackedTarget bestTarget = latestResult.getBestTarget();
+
+    double dz =
+      Units.inchesToMeters(10) / Math.tan((-bestTarget.getPitch() + 24) * Math.PI / 180.0);
+      double dx = dz * Math.tan(bestTarget.getYaw() * Math.PI / 180.0);
+
+      return new Translation2d(dx, dz);
+  }
+
+  public Command driveToNote() {
+    return run(() -> {
+      double headingControllerOutput = this.headingController.calculate(getNoteAngle, 0.0);
+      driveFromInput.(0.0, 1.5, headingControllerOutput, false);
+    }).until(!seesNote()).withTimeout(2.5);
+  //pls commit this
   }
   
 @Override
--- src/main/java/org/robolancers321/BuildConstants.java
+++ src/main/java/org/robolancers321/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "trashbot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "25b60fcdc7b6b24062dbfc9596a2b85462264543";
-  public static final String GIT_DATE = "2024-12-06 17:21:11 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-12-09 19:27:40 EST";
-  public static final long BUILD_UNIX_TIME = 1733790460856L;
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "8c7e977c9050827ffb84dbda58d17cb3221267a5";
+  public static final String GIT_DATE = "2024-12-09 20:13:52 EST";
+  public static final String GIT_BRANCH = "current-branch-12/9";
+  public static final String BUILD_DATE = "2024-12-11 16:40:31 EST";
+  public static final long BUILD_UNIX_TIME = 1733953231091L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
